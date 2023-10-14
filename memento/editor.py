e955b01e4f7530187626426ccf57428ef4f76593
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-10-14 16:48:54 +0330

    imp content setter & getter

--- memento/editor.py
+++ memento/editor.py
@@ -1,9 +1,17 @@
 class Editor:
-    def __init__(self,content)
-        self.content=content
+    def __init__(self, content):
+        self.__content = content
+
+    @property
+    def content(self):
+        return self.__content
+
+    @content.setter
+    def content(self, value):
+        self.__content = value
+
     def create_state(self):
         ...
+
     def restore_state(self):
         ...
-        
-    
