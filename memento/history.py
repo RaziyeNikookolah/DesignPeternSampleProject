b9938589f205ac5da117604e54cb63bf17a0cc40
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-10-14 17:20:46 +0330

    impl history class

--- memento/history.py
+++ memento/history.py
@@ -5,8 +5,11 @@ class History:
     def __init__(self):
         self.states: list[EditerState] = []
 
-    def push(state):
-        ...
+    def push(self, state):
+        self.states.append(state)
 
-    def pop():
-        ...
+    def pop(self):
+        last_index = len(self.states)
+        last_state = self.states[last_index-1]
+        self.states.remove(last_state)
+        return last_state
