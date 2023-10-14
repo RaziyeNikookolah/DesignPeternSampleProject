4c231cabf74319ebc32e941ad72fe9ade9fbd29f
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-10-14 17:14:59 +0330

    impl history class structure

---
+++ memento/history.py
@@ -0,0 +1,12 @@
+from editor_state import EditerState
+
+
+class History:
+    def __init__(self):
+        self.states: list[EditerState] = []
+
+    def push(state):
+        ...
+
+    def pop():
+        ...
